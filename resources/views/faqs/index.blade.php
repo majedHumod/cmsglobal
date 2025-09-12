b27288de18833c1c52876073590ffcb871f7a744
majedHumod <mjd.h2017@gmail.com>
2025-09-12 11:47:21 +0300

    Fix undefined svg key error at line 341

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -310,11 +310,11 @@
                                                 >
                                                     <div class="flex items-center flex-1">
                                                         <div class="w-10 h-10 rounded-lg bg-{{ $categoryData['color'] }}-100 flex items-center justify-center ml-3 text-{{ $categoryData['color'] }}-600">
-                                                            {!! $categoryData['svg'] !!}
+                                                            {!! $categoryData['svg'] ?? '<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>' !!}
                                                         </div>
                                                         <div class="text-right">
                                                             <div class="font-semibold text-sm">{{ $category }}</div>
-                                                            <div class="text-xs text-gray-500 mt-0.5">{{ $categoryData['description'] }}</div>
+                                                            <div class="text-xs text-gray-500 mt-0.5">{{ $categoryData['description'] ?? 'أسئلة متنوعة' }}</div>
                                                         </div>
                                                     </div>
                                                     <div class="flex items-center">
