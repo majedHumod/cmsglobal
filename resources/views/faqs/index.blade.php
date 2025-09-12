f08d2f07206cfefac9e432695d8d3588b4cec694
majedHumod <mjd.h2017@gmail.com>
2025-09-12 11:45:28 +0300

    Updated index.blade.php

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -266,11 +266,7 @@
                                             @foreach($faqs as $category => $categoryFaqs)
                                                 @php
                                                     $categoryIcons = [
-                                                        $categoryData = $categoryIcons[$category] ?? [
-                                                            'svg' => '<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>',
-                                                            'color' => 'gray',
-                                                            'description' => 'أسئلة متنوعة'
-                                                        ];
+                                                        'عام' => [
                                                             'svg' => '<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>',
                                                             'color' => 'orange',
                                                             'description' => 'ابدأ من هنا مع الأساسيات'
@@ -301,7 +297,11 @@
                                                             'description' => 'مساعدة تقنية ودعم فني'
                                                         ]
                                                     ];
-                                                    $categoryData = $categoryIcons[$category] ?? ['svg' => '<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>', 'color' => 'gray', 'description' => 'أسئلة متنوعة'];
+                                                    $categoryData = $categoryIcons[$category] ?? [
+                                                        'svg' => '<svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>',
+                                                        'color' => 'gray',
+                                                        'description' => 'أسئلة متنوعة'
+                                                    ];
                                                 @endphp
                                                 <button 
                                                     @click="activeCategory = '{{ $category }}'"
