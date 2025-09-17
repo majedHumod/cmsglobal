43533b528ff5a28a18b5d5d16f1d2192317ea504
majedHumod <mjd.h2017@gmail.com>
2025-09-18 00:43:46 +0300

    إصلاح خطأ Cache في LandingPageController

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,6 +6,7 @@ use App\Models\LandingPage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class LandingPageController extends Controller
 {
